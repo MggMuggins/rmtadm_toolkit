f9e052c0a5521bb28971b7c8e474c991330a50ee
SamwiseFilmore <mggmugginsmc@gmail.com>
2020-07-03 15:07:11 +0000

    Reorganize server; Argparse; fix gitignore
    Want to have a better cli interface for the server, with colors and all that.

---
+++ server/rmtadm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import json
+from os import path
+import subprocess
+import sys
+
+CONNECTIONS_DIR = "/var/run/rmtadm/"
+
+def connect(client_hostname):
+    client_file = path.join(CONNECTIONS_DIR, client_hostname + ".client")
+
+    with open(client_file) as client:
+        info = json.loads(client.readline().strip())
+
+    ssh = ["ssh", 
+           "-p", info["port"], 
+           "-o", "UserKnownHostsFile /dev/null",
+           "-o", "StrictHostKeyChecking no",
+           info["username"] + "@localhost"]
+
+    subprocess.run(ssh, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+
+def list():
+    os.listdir(CONNECTIONS_DIR)
+
